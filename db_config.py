03257c87708636d800a1a35adc11d50b7ff91d57
Michell Sosa <71848688+Michell-Sosa@users.noreply.github.com>
2021-06-09 20:00:54 -0600

    Adding files via upload
    I had a problem trying to upload the project as we normally do, so I did it via "upload". The error that was shown while I tried to upload the project, appeared when I tried to make the push, it said "Fatal: the remote end hung up unexpectedly" and I didn't knew how to solve it but I didn't wanted to bother you because I know it is late.

---
+++ db_config.py
@@ -0,0 +1,14 @@
+""" Flask configurstion to connect to the database """
+from flask_pymongo import pymongo
+import os
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+client = pymongo.MongoClient(
+    f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@db-example.el6tv.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
+    )
+
+    # add the database name
+db = client.db_example
